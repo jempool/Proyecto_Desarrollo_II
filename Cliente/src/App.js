62b5e068eedee87650ed89ad7c888a87b4cf0b74
jempool <jem.suarez@correounivalle.edu.co>
2019-07-29 18:51:08 -0500

    For the first demo, adding routes: admin_page, user_page, products, categories and subcategories

--- Cliente/src/App.js
+++ Cliente/src/App.js
@@ -12,10 +12,12 @@ export default function App() {
       <AppBar position="static">
         <Toolbar>
 
+          {/* --- Enlace a la ruta de la pagina de bienvenida --- */}
           <TypoGraphy variant="h6" >
             [ <Link to={`/home/`} style={{ textDecoration: 'none', color: 'white' }}> Home</Link>]
           </TypoGraphy>
 
+          {/* --- Enlace a la ruta para el login --- */}
           <TypoGraphy variant="h6" >
             <Link
               to={`/login/`}
@@ -23,6 +25,7 @@ export default function App() {
               &nbsp; &nbsp; &nbsp; Login</Link>
           </TypoGraphy>
 
+          {/* --- Enlace a la ruta para el registro de los usuarios --- */}
           <TypoGraphy variant="h6" >
             <Link
               to={`/Sign_up/`}
@@ -30,6 +33,46 @@ export default function App() {
               &nbsp; &nbsp; &nbsp; Sign up</Link>
           </TypoGraphy>
 
+          {/* --- Enlace a la ruta a la pagina del administrador --- */}
+          <TypoGraphy variant="h6" >
+            <Link
+              to={`/admin_page/`}
+              style={{ textDecoration: 'none', color: 'white' }}>
+              &nbsp; &nbsp; &nbsp; Admin page</Link>
+          </TypoGraphy>
+
+          {/* --- Enlace a la ruta a la pagina del usuario --- */}
+          <TypoGraphy variant="h6" >
+            <Link
+              to={`/user_page/`}
+              style={{ textDecoration: 'none', color: 'white' }}>
+              &nbsp; &nbsp; &nbsp; User page</Link>
+          </TypoGraphy>
+
+          {/* --- Enlace a la ruta a la pagina de los productos --- */}
+          <TypoGraphy variant="h6" >
+            <Link
+              to={`/products/`}
+              style={{ textDecoration: 'none', color: 'white' }}>
+              &nbsp; &nbsp; &nbsp; Products</Link>
+          </TypoGraphy>
+
+          {/* --- Enlace a la ruta a la pagina de las categorias --- */}
+          <TypoGraphy variant="h6" >
+            <Link
+              to={`/categories/`}
+              style={{ textDecoration: 'none', color: 'white' }}>
+              &nbsp; &nbsp; &nbsp; Categories</Link>
+          </TypoGraphy>
+
+          {/* --- Enlace a la ruta a la pagina de las subcategorias --- */}
+          <TypoGraphy variant="h6" >
+            <Link
+              to={`/subcategories/`}
+              style={{ textDecoration: 'none', color: 'white' }}>
+              &nbsp; &nbsp; &nbsp; Subcategories</Link>
+          </TypoGraphy>
+        
         </Toolbar>
       </AppBar>
     </div>
---
+++ Cliente/src/Components/Admin_page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Admin_page extends React.Component {
+    render(){
+      return <h1>Admin page</h1>;
+    }
+  }
