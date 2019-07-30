c5cfe6cb7e1141be73047767a27507ba97b4dca7
STEBAN <steban.cadena@correounivalle.edu.co>
2019-07-30 18:45:35 -0500

    BD V3

--- Servidor/server.js
+++ Servidor/server.js
@@ -53,7 +53,72 @@ app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bo
 /////////////////////////////////////////////////////
 ///////////CONSULTAS DE LOS PRODUCTOS////////////////
 /////////////////////////////////////////////////////
+
+//Insertar productos en la base de datos
 app.post("/insertProduct", function(req,res){
+
+  str='INSERT INTO book VALUES ('+req.body.isbn+',\''+ req.body.name_subcategory +'\',\''+ req.body.publication_year +'\',\''+ req.body.synopsis +'\',\''+ req.body.title +'\',\''+ req.body.author +'\','+ req.body.number_of_pages +','+ req.body.price +',\''+ req.body.editorial +'\',\''+ req.body.edition +'\',\''+ req.body.lang +'\',\''+ req.body.cover_type +'\',\''+ req.body.recommended_age+'\')';    
+  console.log(str)
+  connect(function(err, client, done) {
+  client.query(str,(err, result)=> {
+      //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+      done(err);
+      if(err) {
+        res.json([{bool: false }]);
+        return console.error('error running query', err);
+      }
+      else{
+        res.json([{bool: true }]);
+        console.log("funciono?");
+      }
+  });
+});
+})
+
+//Consultar productos de la base de datos
+app.get("/getProduct", function(req,res){
+
+  str='INSERT INTO book VALUES ('+req.body.isbn+',\''+ req.body.name_subcategory +'\',\''+ req.body.publication_year +'\',\''+ req.body.synopsis +'\',\''+ req.body.title +'\',\''+ req.body.author +'\','+ req.body.number_of_pages +','+ req.body.price +',\''+ req.body.editorial +'\',\''+ req.body.edition +'\',\''+ req.body.lang +'\',\''+ req.body.cover_type +'\',\''+ req.body.recommended_age+'\')';    
+  console.log(str)
+  connect(function(err, client, done) {
+  client.query(str,(err, result)=> {
+      //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+      done(err);
+      if(err) {
+        res.json([{bool: false }]);
+        return console.error('error running query', err);
+      }
+      else{
+        res.json([{bool: true }]);
+        console.log("funciono?");
+      }
+  });
+});
+})
+
+//Modificar productos de la base de datos
+app.post("/updateProduct", function(req,res){
+
+  str='INSERT INTO book VALUES ('+req.body.isbn+',\''+ req.body.name_subcategory +'\',\''+ req.body.publication_year +'\',\''+ req.body.synopsis +'\',\''+ req.body.title +'\',\''+ req.body.author +'\','+ req.body.number_of_pages +','+ req.body.price +',\''+ req.body.editorial +'\',\''+ req.body.edition +'\',\''+ req.body.lang +'\',\''+ req.body.cover_type +'\',\''+ req.body.recommended_age+'\')';    
+  console.log(str)
+  connect(function(err, client, done) {
+  client.query(str,(err, result)=> {
+      //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+      done(err);
+      if(err) {
+        res.json([{bool: false }]);
+        return console.error('error running query', err);
+      }
+      else{
+        res.json([{bool: true }]);
+        console.log("funciono?");
+      }
+  });
+});
+})
+
+//Eliminar productos de la base de datos
+app.delete("/deleteProduct", function(req,res){
 
   str='INSERT INTO book VALUES ('+req.body.isbn+',\''+ req.body.name_subcategory +'\',\''+ req.body.publication_year +'\',\''+ req.body.synopsis +'\',\''+ req.body.title +'\',\''+ req.body.author +'\','+ req.body.number_of_pages +','+ req.body.price +',\''+ req.body.editorial +'\',\''+ req.body.edition +'\',\''+ req.body.lang +'\',\''+ req.body.cover_type +'\',\''+ req.body.recommended_age+'\')';    
   console.log(str)
