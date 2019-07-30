d638b05021529faa5c326f10ef6ed2d81a5e9012
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2019-07-30 18:05:06 -0500

    inicio del HU

--- Servidor/server.js
+++ Servidor/server.js
@@ -50,6 +50,36 @@ app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bo
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Actualiza los datos de un determinado usuario en la base de datos
+app.delete("/eliminarCategorias", function (req, res) {
+
+  let str = "DELETE FROM category WHERE name_category='$1' ;"
+
+  let vars = [req.body.category]
+
+  console.log(vars)
+
+    connect(function(err, client, done) {
+      if(err) {
+          return console.error('error fetching client from pool', err);
+      }
+      //use the client for executing the query
+  
+      client.query(str,vars,(err, result) =>{
+
+        console.log(str)
+        //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
+        done(err);
+        if(err) {
+          res.json([{bool:false}]);
+          return console.error('error running query', err);
+        }
+        else{
+          res.json([{bool:true}]);
+        }
+      });
+    });
+  });
 
 
 /////////////////////////////////////////////////////
