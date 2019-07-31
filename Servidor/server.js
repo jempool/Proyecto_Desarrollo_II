624f6eb948fa6eb42b3c434ad64196f8c7b42028
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2019-07-30 23:38:33 -0500

    bug corregido

--- Servidor/server.js
+++ Servidor/server.js
@@ -15,7 +15,7 @@ var config = {
   database: 'Library', //env var: PGDATABASE
   password: '1234', //env var: PGPASSWORD
   host: 'localhost', // Server hosting the postgres database
-  port: 5431, //env var: PGPORT
+  port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
