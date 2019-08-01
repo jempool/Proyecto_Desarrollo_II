9fb103a3a40c18eb4eab883b0634f6c16fe3709e
jempool <jem.suarez@correounivalle.edu.co>
2019-08-01 10:31:13 -0500

    list/deactivate users

--- Cliente/src/Components/Admin_page/Admin_page.js
+++ Cliente/src/Components/Admin_page/Admin_page.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Dashboard from "./Dashboard"
 import { JsonToTable } from "react-json-to-table";
+import { Table } from "@material-ui/core";
+import "./Admin_page.css"
 
 
 class Admin_page extends Component {
@@ -19,7 +21,8 @@ class Admin_page extends Component {
             type_id: "", id: "", phone_number: "", address: "",
             email: "", state: ""
           }]
-      }
+      },
+      textBox: ""
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -27,30 +30,65 @@ class Admin_page extends Component {
   //   console.log(e); return;
   // };
 
-  handleClick() {
-    console.log("hola");
-    fetch("/customers", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ Username: "jem", Password: "123" })
-    })
+  handleClick(e) {
 
+    if (e.target.id === "desactivar") {
+      console.log(this.state.textBox);
+      fetch("/customers", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ action: e.target.id, client: this.state.textBox })
+      })
       .then(res => res.json())
       .then(res => {
-        
         this.setState({ algo: "" });
         this.setState({ algo: res[0] });
-        
-        console.log(this.state.algo.client[0].state);
-      });
+      }); 
+    }
 
+    if (e.target.id === "cliente") {
+      fetch("/customers", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ action: e.target.id, client: "" })
+      })
+
+        .then(res => res.json())
+        .then(res => {
+
+          this.setState({ algo: "" });
+          this.setState({ algo: res[0] });
+
+          //console.log(this.state.algo.client[0].state);
+        });
+    } 
+  }
+  
+  updateInputValue = (evt) => {
+    this.setState({
+      textBox: evt.target.value
+    });
   }
 
 
   render() {
+
+
+    const tab = (
+      <tbody>
+        <tr>
+          <td >1</td>
+          <td>2</td>
+          <button>b</button>
+        </tr>
+      </tbody>);
+
     return (
       // <div>
       //   <Dashboard handleList={this.handleList}/>
@@ -63,11 +101,40 @@ class Admin_page extends Component {
       //   </div>);
 
       <div>
-        <button onClick={this.handleClick}>Clientes</button>
+        <button id="cliente" onClick={this.handleClick}>Clients</button>
         <JsonToTable json={this.state.algo} />
         <br />
-        <button onClick={console.log("Desactivar")}>Desactivar</button>
+        <button id="desactivar" onClick={this.handleClick }>activate/deactivate</button>
+        <input type="text" onChange={this.updateInputValue} />
+
+
+        {/* <Table id="table" border="1">
+          <th>username</th>  <th>first_name</th> <th>last_name</th>
+          <th>date_birth</th> <th>type_id</th> <th>id</th>
+          <th>phone_number</th> <th>address</th> <th>email</th>
+          <th>state</th><th>change</th>
+
+{tab} */}
+
+        {/* <tr>
+            <td >1</td>
+            <td>2</td>
+            <button>b</button>
+          </tr>
+
+          <tr>
+            <td>3</td>
+            <td>4</td>
+            <button>b</button>
+          </tr> */}
+
+        {/* </Table> */}
+
+
+
+
       </div>);
+
 
   }
 }
