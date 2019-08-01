87ff6aac6f6ba6965a16aba9f26897881e6af724
stebanca1998 <steban.cadena@correounivalle.edu.co>
2019-08-01 01:54:48 -0500

    Insertar y eliminar productos sin diseno

--- Cliente/src/Components/Products.js
+++ Cliente/src/Components/Products.js
@@ -7,27 +7,27 @@ export default class Products extends React.Component {
     super(props);
 
     this.state={
-      isbn : 45747,
-      name_subcategory: "",
-      publication_year: "",
-      synopsis: "",
-      title: "",
-      author: "",
-      number_of_pages: 342,
-      price: 345421, 
-      editorial: "",
-      edition: "",
-      lang: "",
-      cover_type: "",
-      recommended_age: 15,
-      tipo: "inicio" 
+      isbn : '',
+      name_subcategory: '',
+      publication_year: '',
+      synopsis: '',
+      title: '',
+      author: '',
+      number_of_pages:'',
+      price: '', 
+      editorial: '',
+      edition: '',
+      lang: '',
+      cover_type: '',
+      recommended_age:'',
+      tipo: 'inicio' 
     }
 
 
     this.insertpro = this.insertpro.bind(this);
-    this.insertpro = this.getpro.bind(this);
-    this.insertpro = this.updatepro.bind(this);
-    this.insertpro = this.deletepro.bind(this);
+    this.getpro = this.getpro.bind(this);
+    this.updatepro = this.updatepro.bind(this);
+    this.deletepro = this.deletepro.bind(this);
 
     this.handleClick = this.handleClick.bind(this);
     this.action = this.action.bind(this);
@@ -69,23 +69,78 @@ export default class Products extends React.Component {
         }
       }
       )
-    
+
   }
 
   getpro(){
-
+    fetch("/getProduct",{
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        isbn : this.state.isbn,
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if(res[0].bool){
+        console.log("Creo que funciona");
+      }
+      else{
+        console.log("Creo que no funciona");
+      }
+    }
+    )
   }
 
   updatepro(){
-
+    fetch("/updateProduct",{
+      method: "PUT",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        isbn : this.state.isbn,
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if(res[0].bool){
+        console.log("Creo que funciona");
+      }
+      else{
+        console.log("Creo que no funciona");
+      }
+    }
+    )
   }
 
   deletepro(){
-
+    fetch("/deleteProduct/"+this.state.isbn,{
+      method: "DELETE",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(res => {
+      if(res[0].bool){
+        console.log("Creo que funciona");
+      }
+      else{
+        console.log("Creo que no funciona");
+      }
+    }
+    )
   }
 
   handleClick(e){
-        this.setState({tipo: e.target.name});
+        console.log(e.target.value);
+        this.setState({tipo: e.target.value});
   }
 
   actualizarDatos(e){
@@ -166,52 +221,82 @@ export default class Products extends React.Component {
       case "insertar":
         return(
           <div>
+            <hr/>
+            <p>Inserte los datos del producto que desea agregar</p>
             <form>
-              <Input name="isbn" type="text" placeholder='ISBN*' onChange={this.actualizarDatos} value={this.state.isbn}/>
-              <Input name="name_subcategory" type="text" placeholder='Subcategoria*' onChange={this.actualizarDatos} value={this.state.name_subcategory}/>
-              <Input name="publication_year" type="text" placeholder='Año*' onChange={this.actualizarDatos} value={this.state.publication_year}/>
-              <Input name="synopsis" type="text" placeholder='Sipnosis*' onChange={this.actualizarDatos} value={this.state.synopsis}/>
-              <Input name="title" type="text" placeholder='Titulo*' onChange={this.actualizarDatos} value={this.state.title}/>
-              <Input name="author" type="text" placeholder='Autor*' onChange={this.actualizarDatos} value={this.state.author}/>
-              <Input name="number_of_pages" type="text" placeholder='Páginas*' onChange={this.actualizarDatos} value={this.state.number_of_pages}/>
-              <Input name="price" type="text" placeholder='Precio*' onChange={this.actualizarDatos} value={this.state.price}/>
-              <Input name="editorial" type="text" placeholder='Editorial*' onChange={this.actualizarDatos} value={this.state.editorial}/>
-              <Input name="edition" type="text" placeholder='Edición*' onChange={this.actualizarDatos} value={this.state.edition}/>
-              <Input name="lang" type="text" placeholder='Lenguaje*' onChange={this.actualizarDatos} value={this.state.lang}/>
-              <Input name="cover_type" type="text" placeholder='Tipo de cubierta*' onChange={this.actualizarDatos} value={this.state.cover_type}/>
-              <Input name="recommended_age" type="text" placeholder='Edad recomedada*' onChange={this.actualizarDatos} value={this.state.recommended_age}/>
+              <Input name="isbn" type="text" placeholder='ISBN*' onChange={this.actualizarDatos} value={this.state.isbn}/> <br/>
+              <Input name="name_subcategory" type="text" placeholder='Subcategoria*' onChange={this.actualizarDatos} value={this.state.name_subcategory}/> <br/>
+              <Input name="publication_year" type="text" placeholder='Año*' onChange={this.actualizarDatos} value={this.state.publication_year}/> <br/>
+              <Input name="synopsis" type="text" placeholder='Sipnosis*' onChange={this.actualizarDatos} value={this.state.synopsis}/> <br/>
+              <Input name="title" type="text" placeholder='Titulo*' onChange={this.actualizarDatos} value={this.state.title}/> <br/>
+              <Input name="author" type="text" placeholder='Autor*' onChange={this.actualizarDatos} value={this.state.author}/> <br/>
+              <Input name="number_of_pages" type="text" placeholder='Páginas*' onChange={this.actualizarDatos} value={this.state.number_of_pages}/> <br/>
+              <Input name="price" type="text" placeholder='Precio*' onChange={this.actualizarDatos} value={this.state.price}/> <br/>
+              <Input name="editorial" type="text" placeholder='Editorial*' onChange={this.actualizarDatos} value={this.state.editorial}/> <br/>
+              <Input name="edition" type="text" placeholder='Edición*' onChange={this.actualizarDatos} value={this.state.edition}/> <br/>
+              <Input name="lang" type="text" placeholder='Lenguaje*' onChange={this.actualizarDatos} value={this.state.lang}/><br/> 
+              <Input name="cover_type" type="text" placeholder='Tipo de cubierta*' onChange={this.actualizarDatos} value={this.state.cover_type}/><br/>
+              <Input name="recommended_age" type="text" placeholder='Edad recomedada*' onChange={this.actualizarDatos} value={this.state.recommended_age}/> <br/>
+              <Button type="submit" onClick={this.insertpro}>Insertar producto</Button> <br/>
             </form>
           </div>
         );
-        break;
       case "obtener":
         return(
           <div>
+            <hr/>
+            <p>Inserte el isbn del producto que desea consultar </p>
             <form>
-              
+              <Input name="isbn" type="text" placeholder='ISBN*' onChange={this.actualizarDatos} value={this.state.isbn}/>
+              <Button onClick={this.getpro}>Consultar producto</Button> <br/>
+              <Input name="name_subcategory" type="text" disabled placeholder='Subcategoria*' onChange={this.actualizarDatos} value={this.state.name_subcategory}/> <br/>
+              <Input name="publication_year" type="text" disabled placeholder='Año*' onChange={this.actualizarDatos} value={this.state.publication_year}/> <br/>
+              <Input name="synopsis" type="text" disabled placeholder='Sipnosis*' onChange={this.actualizarDatos} value={this.state.synopsis}/> <br/>
+              <Input name="title" type="text" disabled placeholder='Titulo*' onChange={this.actualizarDatos} value={this.state.title}/> <br/>
+              <Input name="author" type="text" disabled placeholder='Autor*' onChange={this.actualizarDatos} value={this.state.author}/> <br/>
+              <Input name="number_of_pages" type="text" disabled placeholder='Páginas*' onChange={this.actualizarDatos} value={this.state.number_of_pages}/> <br/>
+              <Input name="price" type="text" disabled placeholder='Precio*' onChange={this.actualizarDatos} value={this.state.price}/> <br/>
+              <Input name="editorial" type="text" disabled placeholder='Editorial*' onChange={this.actualizarDatos} value={this.state.editorial}/> <br/>
+              <Input name="edition" type="text" disabled placeholder='Edición*' onChange={this.actualizarDatos} value={this.state.edition}/> <br/>
+              <Input name="lang" type="text" disabled placeholder='Lenguaje*' onChange={this.actualizarDatos} value={this.state.lang}/><br/> 
+              <Input name="cover_type" type="text" disabled placeholder='Tipo de cubierta*' onChange={this.actualizarDatos} value={this.state.cover_type}/><br/>
+              <Input name="recommended_age" type="text" disabled placeholder='Edad recomedada*' onChange={this.actualizarDatos} value={this.state.recommended_age}/> <br/>
             </form>
           </div>
         );
-        break;
       case "actualizar":
         return(
           <div>
+            <hr/>
+            <p>Inserte los datos del producto que desea modificar</p>
             <form>
-              
-            </form>            
+              <Input name="isbn" type="text" placeholder='ISBN*' onChange={this.actualizarDatos} value={this.state.isbn}/> 
+              <Button onClick={this.getpro}>Cargar producto</Button> <br/>
+              <Input name="name_subcategory" type="text" placeholder='Subcategoria*' onChange={this.actualizarDatos} value={this.state.name_subcategory}/> <br/>
+              <Input name="publication_year" type="text" placeholder='Año*' onChange={this.actualizarDatos} value={this.state.publication_year}/> <br/>
+              <Input name="synopsis" type="text" placeholder='Sipnosis*' onChange={this.actualizarDatos} value={this.state.synopsis}/> <br/>
+              <Input name="title" type="text" placeholder='Titulo*' onChange={this.actualizarDatos} value={this.state.title}/> <br/>
+              <Input name="author" type="text" placeholder='Autor*' onChange={this.actualizarDatos} value={this.state.author}/> <br/>
+              <Input name="number_of_pages" type="text" placeholder='Páginas*' onChange={this.actualizarDatos} value={this.state.number_of_pages}/> <br/>
+              <Input name="price" type="text" placeholder='Precio*' onChange={this.actualizarDatos} value={this.state.price}/> <br/>
+              <Input name="editorial" type="text" placeholder='Editorial*' onChange={this.actualizarDatos} value={this.state.editorial}/> <br/>
+              <Input name="edition" type="text" placeholder='Edición*' onChange={this.actualizarDatos} value={this.state.edition}/> <br/>
+              <Input name="lang" type="text" placeholder='Lenguaje*' onChange={this.actualizarDatos} value={this.state.lang}/><br/> 
+              <Input name="cover_type" type="text" placeholder='Tipo de cubierta*' onChange={this.actualizarDatos} value={this.state.cover_type}/><br/>
+              <Input name="recommended_age" type="text" placeholder='Edad recomedada*' onChange={this.actualizarDatos} value={this.state.recommended_age}/> <br/>
+              <Button type="submit" onClick={this.updatepro}>Insertar producto</Button> <br/>
+            </form>          
           </div>
         );
-        break;
       case "eliminar":
         return(
           <div>
-            <form>
-              
-            </form> 
+            <hr/>
+            <p>Inserte el isbn del producto que desea eliminar</p>
+            <Input name="isbn" type="text" placeholder='ISBN*' onChange={this.actualizarDatos} value={this.state.isbn}/> <br/>
+            <Button type="submit" onClick={this.deletepro}>Eliminar producto</Button>
           </div>
         );
-        break;
-
       default:
         break;
     }
@@ -221,11 +306,11 @@ export default class Products extends React.Component {
       return (
         <div className='botns'>
         <h1>Products</h1>
-        <Button name="insertar" onClick={this.handleClick}>INSERTAR PRODUCTOS</Button>
-        <Button name="obtener" onClick={this.handleClick}>OBTENER PRODUCTOS</Button>
-        <Button name="actualizar" onClick={this.handleClick}>ACTUALIZAR PRODUCTOS</Button>
-        <Button name="eliminar" onClick={this.handleClick}>ELIMINAR PRODUCTOS</Button>
-        {this.action}
+        <Button onClick={() => this.setState({tipo: "insertar"})}>INSERTAR PRODUCTOS</Button>
+        <Button onClick={() => this.setState({tipo: "obtener"})}>OBTENER PRODUCTOS</Button>
+        <Button onClick={() => this.setState({tipo: "actualizar"})}>ACTUALIZAR PRODUCTOS</Button>
+        <Button onClick={() => this.setState({tipo: "eliminar"})}>ELIMINAR PRODUCTOS</Button>
+        {this.action()}
       </div>
       );
     }
