4208480502d4fcc449e38510a281a171b26a96e8
stebanca1998 <steban.cadena@correounivalle.edu.co>
2019-08-05 01:51:16 -0500

    orms clientes, productos, ventas

--- Servidor/server.js
+++ Servidor/server.js
@@ -10,7 +10,7 @@ const Pool = require('pg-pool');
 ////////////////////////////////////////////
 //////////CONFIGURACION DEL ORM ////////////
 ////////////////////////////////////////////
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('Library', 'postgres', '1234', {
   host: 'localhost',
@@ -59,7 +59,7 @@ pool.on('error', function (err, client) {
 //export the query method for passing queries to the pool
 function query(text, values, callback) {
 
-  return pool.query(text, values, callback);
+  return pool.query(text, values, callback );
 };
 
 // the pool also supports checking out a client for
@@ -74,6 +74,160 @@ app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+const Book = sequelize.define ('book',{
+    isbn:{
+        type: Sequelize.BIGINT,
+        primaryKey: true 
+    },
+    name_subcategory:{
+        type: Sequelize.TEXT,
+    },
+    publication_year:{
+        type: Sequelize.TEXT,
+        allowNull: false        
+    },
+    synopsis:{
+        type: Sequelize.TEXT,
+        allowNull: false        
+    },
+    title:{
+        type: Sequelize.TEXT,
+        allowNull: false             
+    },
+    author:{
+        type: Sequelize.TEXT,
+        allowNull: false             
+    },
+    number_of_pages:{
+        type: Sequelize.INTEGER,
+        allowNull: false       
+    },
+    price:{
+        type: Sequelize.BIGINT,
+        allowNull: false             
+    },
+    editorial:{
+        type: Sequelize.TEXT,
+        allowNull: false             
+    },
+    edition:{
+        type: Sequelize.TEXT,
+        allowNull: false       
+    },
+    lang:{
+        type: Sequelize.TEXT,
+        allowNull: false       
+    },
+    cover_type:{
+        type: Sequelize.STRING(1),
+        allowNull: false             
+    },
+    recommended_age:{
+        type: Sequelize.TEXT,
+        allowNull: false             
+    }
+},{
+    freezeTableName: true,
+    timestamps : false
+})
+
+//Modelo de los clientes
+const User = sequelize.define ('client',{
+  username:{
+      type: Sequelize.TEXT,
+      primaryKey: true 
+  },
+  first_name:{
+      type: Sequelize.TEXT,
+      allowNull: false        
+  },
+  last_name:{
+      type: Sequelize.TEXT,
+      allowNull: false        
+  },
+  date_birth:{
+      type: Sequelize.DATEONLY,
+      allowNull: false        
+  },
+  type_id:{
+      type: Sequelize.TEXT(2),
+      allowNull: false             
+  },
+  id:{
+      type: Sequelize.BIGINT,
+      allowNull: false             
+  },
+  password:{
+      type: Sequelize.TEXT,
+      allowNull: false       
+  },
+  phone_number:{
+      type: Sequelize.BIGINT,
+      allowNull: false             
+  },
+  gender:{
+      type: Sequelize.TEXT(1),
+      allowNull: false             
+  },
+  address:{
+      type: Sequelize.TEXT,
+      allowNull: false       
+  },
+  email:{
+      type: Sequelize.TEXT,
+      allowNull: false       
+  },
+  credit_card_number:{
+      type: Sequelize.BIGINT,
+      allowNull: false             
+  },
+  state:{
+      type: Sequelize.BOOLEAN,
+      allowNull: false             
+  }
+},{
+  freezeTableName: true,
+  timestamps : false
+})
+
+//Modelo de las ventas
+const Bill = sequelize.define ('bill',{
+  id_bill:{
+      type: Sequelize.BIGINT,
+      primaryKey: true,
+      autoIncrement: true
+  },
+  username:{
+      type: Sequelize.TEXT,
+      allowNull: false        
+  },
+  date:{
+      type: Sequelize.DATE,
+      allowNull: false        
+  }
+},{
+  freezeTableName: true,
+  timestamps : false
+})
+
+//Modelo de los productos de una venta
+const BillBook = sequelize.define ('bill_book',{
+  id_bill:{
+      type: Sequelize.BIGINT,
+      primaryKey: true
+  },
+  isbn:{
+      type: Sequelize.BIGINT,
+      allowNull: true        
+  },
+  quantity:{
+      type: Sequelize.INTEGER,
+      allowNull: false        
+  }
+},{
+  freezeTableName: true,
+  timestamps : false
+})
 
 /////////////////////////////////////////////////////
 ///////////CONSULTAS DE LOS PRODUCTOS////////////////
@@ -82,94 +236,236 @@ app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bo
 //Insertar productos en la base de datos
 app.post("/insertProduct", function(req,res){
 
-  let str='INSERT INTO book VALUES ($1, $2 , $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13 );';    
-  let vars = [req.body.isbn,req.body.name_subcategory, req.body.publication_year, req.body.synopsis, req.body.title, req.body.author,req.body.number_of_pages, req.body.price, req.body.editorial, req.body.edition , req.body.lang , req.body.cover_type, req.body.recommended_age];  
+  Book.create(req.body).then(x => res.json(x))
+  .catch(err => console.log(err));
 
-  console.log(str);
-  connect(function(err, client, done) {
-  client.query(str,vars,(err, result)=> {
-      //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
-      done(err);
-      if(err) {
-        res.json([{bool: false }]);
-        return console.error('error running query', err);
-      }
-      else{
-        res.json([{bool: true }]);
-        console.log("funciono?");
-      }
-  });
-});
 })
 
 //Consultar productos de la base de datos
 app.post('/getProduct', function(req,res){
 
-  let str="SELECT * FROM book WHERE isbn= $1;";
-  let vars=[req.body.isbn];
+  Book.findAll({where: {
+    isbn: req.body.isbn
+  }})
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
 
-  console.log(str);
-  connect(function(err, client, done) {
-    client.query(str, vars,(err, result)=> {
-        //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
-        done(err);
-        if(err) {
-          res.json([{bool: false }]);
-          return console.error('error running query', err);
-        }
-        else{
-          res.json(result.rows);
-          console.log("funciono?");
-        }
-    });
-  });
 })
 
 //Modificar los datos de un producto especifico de la base de datos
 app.put("/updateProduct", function(req,res){
 
-  let str="UPDATE book SET name_subcategory=$2, publication_year=$3, synopsis=$4, title=$5, author=$6, number_of_pages=$7, price=$8, editorial=$9, edition=$10, lang=$11, cover_type=$12, recommended_age=$13 WHERE isbn=$1;";
-  let vars = [req.body.isbn,req.body.name_subcategory, req.body.publication_year, req.body.synopsis, req.body.title, req.body.author,req.body.number_of_pages, req.body.price, req.body.editorial, req.body.edition , req.body.lang , req.body.cover_type, req.body.recommended_age];  
+let index = req.body.isbn;
+delete req.body.isbn
 
-  console.log(str);
-  connect(function(err, client, done) {
-  client.query(str,vars,(err, result)=> {
-      //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
-      done(err);
-      if(err) {
-        res.json([{bool: false }]);
-        return console.error('error running query', err);
-      }
-      else{
-        res.json([{bool: true }]);
-        console.log("funciono?");
-      }
-  });
-});
+Book.update(req.body,{where: {
+  isbn: index
+}}).then(x => res.json(x))
+.catch(err => console.log(err));
 })
 
 //Eliminar un producto especifico de la base de datos
 app.delete('/deleteProduct/:isbn', function(req,res){
 
-  let str="DELETE FROM book WHERE isbn= $1;";
-  let vars=[req.params.isbn];
+  Book.destroy({where: {
+    isbn: req.params.isbn
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
 
-  console.log(str);
-  connect(function(err, client, done) {
-  client.query(str, vars,(err, result)=> {
-      //call `done(err)` to release the client back to the pool (or destroy it if there is an error)
-      done(err);
-      if(err) {
-        res.json([{bool: false }]);
-        return console.error('error running query', err);
-      }
-      else{
-        res.json([{bool: true }]);
-        console.log("funciono?");
-      }
-  });
-});
 })
+
+/////////////////////////////////////////////////////
+///////////CONSULTAS DE LOS CLIENTES/////////////////
+/////////////////////////////////////////////////////
+
+//insertar un cliente
+app.post("/insertClient",function(req,res){
+
+  User.create(req.body).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar cliente
+
+app.post("/getClient",function(req,res){
+
+  User.findAll({where: {
+    username: req.body.username
+  }})
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar clientes
+
+app.post("/getClients",function(req,res){
+
+  User.findAll()
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar clientes filtrados por genero
+
+app.post("/getClientsg",function(req,res){
+
+  User.findAll({where: {
+    gender: req.body.gender
+  }})
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Modificar cliente
+
+app.put("/updateClient", function(req,res){
+  
+  let index = req.body.username;
+  delete req.body.username
+  
+  User.update(req.body,{where: {
+    username: index
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Eliminar cliente
+
+app.delete("/deleteClient/:username", function(req,res){
+
+  User.destroy({where: {
+    username: req.params.username
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+/////////////////////////////////////////////////////
+////////////CONSULTAS DE LAS VENTAS//////////////////
+/////////////////////////////////////////////////////
+
+//insertar una venta
+app.post("/insertBill",function(req,res){
+
+  Bill.create(req.body).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar las ventas
+
+app.post("/getBills",function(req,res){
+
+  Bill.findAll()
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar una venta
+
+app.post("/getBill",function(req,res){
+
+  Bill.findAll({where: {
+    id_bill: req.body.id_bill
+  }})
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Modificar una venta
+
+app.put("/updateBill", function(req,res){
+  
+  let index = req.body.id_bill;
+  delete req.body.id_bill
+  
+  Bill.update(req.body,{where: {
+    id_bill: index
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Eliminar una venta
+
+app.delete("/deleteClient/:idbill", function(req,res){
+
+  Bill.destroy({where: {
+    id_bill: req.params.idbill
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+/////////////////////////////////////////////////////
+///////CONSULTAS DE LOS PODUCTOS DE VENTAS///////////
+/////////////////////////////////////////////////////
+
+//insertar un producto a un venta
+app.post("/insertBillbook",function(req,res){
+
+  BillBook.create(req.body).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar un producto en una venta especifica
+
+app.post("/getBillBookv",function(req,res){
+
+  BillBook.findAll({where: {
+    id_bill: req.body.id_bill
+  }})
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Consultar las ventas de un producto especifico
+app.post("/getBillBookp",function(req,res){
+
+  BillBook.findAll({where: {
+    isbn: req.body.isbn
+  }})
+  .then(x =>  res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Modificar unproducto en una venta
+
+app.put("/updateBillBook", function(req,res){
+  
+  BillBook.update(req.body.quantity,{where: {
+    id_bill: req.body.id_bill,
+    isbn: req.body.isbn
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+//Eliminar un producto en una venta
+
+app.delete("/deleteBillBook/:idbill-:isbn", function(req,res){
+
+  BillBook.destroy({where: {
+    id_bill: req.params.idbill,
+    isbn: req.params.isbn
+  }}).then(x => res.json(x))
+  .catch(err => console.log(err));
+
+})
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
 
 app.post("/crearSubCategorias", function (req, res) {
 
@@ -379,110 +675,6 @@ app.post("/customers", function (req, res) {
   }
 
 });
-
-/////////////////////////////////////////////////////
-/////////////////CRUD DEL CLIENTE////////////////////
-/////////////////////////////////////////////////////
-
-//insertar un cliente
-app.post("/insertClient",function(req,res){
-
-  let str = 'INSERT INTO client VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)'
-  let vars = [req.body.username,req.body.first_name, req.body.last_name, req.body.date_birth, req.body.type_id, req.body.id, req.body.password, req.body.phone_number, req.body.address, req.body.email, req.body.credit_card_number, req.body.state]
-
-
-  connect(function(err,client,done){
-    client.query(str,vars,(err, result)=> {
-      done(err);
-      if(err){
-        res.json([{bool:false}]);
-        return console.error('error running query',err);
-      }
-      else{
-        res.json([{bool:true}]);
-
-      }
-    });
-  });
-})
-
-//Consultar cliente
-
-app.post("/getClient",function(req,res){
-
-  let str = "SELECT * FROM client WHERE username = $1;";
-  let vars = [req.body.username]
-
-  console.log(str);
-  connect(function(err,client,done){
-
-    client.query(str, vars,(err,result) => {
-
-      done(err);
-      if(err){
-        res.json([{bool:false}]);
-        return console.error('error runinng query', err);
-      }
-      else{
-
-        res.json([{bool:true}]);
-        console.log(res);
-      }
-    });
-  });
-})
-
-//Modificar cliente
-
-app.put("/updateClient", function(req,res){
-
-  let str = "UPDATE client SET first_name=$2, last_name=$3, date_birth=$4, type_id=$5, id=$6, password=$7, phone_number=$8, address=$9, email=$10, credit_card_number=$11, state=$12 WHERE username = $1;";
-  let vars = [req.body.username, req.body.first_name, req.body.last_name, req.body.date_birth, req.body.type_id, req.body.id, req.body.password, req.body.phone_number, req.body.address, req.body.email, req.body.credit_card_number, req.body.state];
-
-  console.log(str)
-  connect(function(err,client,done){
-
-    client.query(str,vars,(err,result)=>{
-
-      done(err);
-      if(err){
-        res.json([{bool:false}]);
-        return console.error('error runinng query', err);
-      }
-      else{
-
-        res.json([{bool:true}]);
-        console.log(":v");
-      }
-    });
-  });
-})
-
-//Eliminar cliente
-
-app.delete("/deleteClient/:username", function(req,res){
-
-  let str = "DELETE FROM client WHERE username=$1;";
-  let vars = [req.params.username];
-
-  console.log(str);
-  connect(function(err,client,done){
-    client.query(str,vars,(err,result) => {
-
-      done(err);
-      if(err){
-        res.json([{bool:false}]);
-        return console.error('error runinng query', err);
-      }
-      else{
-
-        res.json([{bool:true}]);
-        console.log(":v");
-      }
-    })
-  })
-})
-
 
 /////////////////////////////////////////////////////
 ////////////CONFIGURACION DEL PUERTO ////////////////
