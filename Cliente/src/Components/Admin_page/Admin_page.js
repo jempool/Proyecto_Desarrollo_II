50025cd92abd97b86822fd387324acfbd3b5fdda
jempool <jem.suarez@correounivalle.edu.co>
2019-08-08 19:09:19 -0500

    commit

--- Cliente/src/Components/Admin_page/Admin_page.js
+++ Cliente/src/Components/Admin_page/Admin_page.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import {Select} from '@material-ui/core';
+import { Select } from '@material-ui/core';
 import { JsonToTable } from "react-json-to-table";
 import "./Admin_page.css"
+import Dashboard from "./Dashboard";
 
 
 class Admin_page extends Component {
@@ -11,7 +12,7 @@ class Admin_page extends Component {
       algo: {
         Client: []
       },
-      status:"Select"
+      status: "Select"
     };
     this.handleClick = this.handleClick.bind(this);
     this.getClient = this.getClient.bind(this);
@@ -19,19 +20,19 @@ class Admin_page extends Component {
   }
 
 
-  getClient(){
+  getClient() {
     fetch("/Client/consult", {
       method: "GET",
     })
 
       .then(res => res.json())
-      .then(res => {        
-        this.setState({ algo: {Client:[]} });
+      .then(res => {
+        this.setState({ algo: { Client: [] } });
         this.setState({ algo: res[0] });
       });
 
   }
- 
+
 
   handleClick(e) {
 
@@ -41,18 +42,18 @@ class Admin_page extends Component {
         Accept: "application/json, text/plain, */*",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({client: this.state.status })
+      body: JSON.stringify({ client: this.state.status })
     })
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-      this.setState({ algo: {Client:[]} });
-    })
-    .then(()=>{
-      this.getClient()
-    }); 
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        this.setState({ algo: { Client: [] } });
+      })
+      .then(() => {
+        this.getClient()
+      });
   }
-  
+
   updateInputValue = (evt) => {
     this.setState({
       textBox: evt.target.value
@@ -62,76 +63,31 @@ class Admin_page extends Component {
 
   render() {
     console.log(this.state.algo)
-
-/*
-    const tab = (
-      <tbody>
-        <tr>
-          <td >1</td>
-          <td>2</td>
-          <button>b</button>
-        </tr>
-    </tbody>);*/
-
     return (
-      // <div>
-      //   <Dashboard handleList={this.handleList}/>
-      //   <button onClick={this.handleClick}>Clientes</button>
-      //   <ul>
-      //   {result.map(item => {
-      //       return <li>{item[1]}</li>;
-      //     })}
-      //   </ul>
-      //   </div>);
-
       <div>
-        <JsonToTable json={this.state.algo} />
-        <br />
-        <button id="desactivar" onClick={this.handleClick }>activate/deactivate</button>
-        <Select 
-                    name="categoryName"
-                    value={this.state.status}
-                    onChange={(x)=>this.setState({status:x.target.value})}
-                    placeholder="Selecciona una subcategoria:"
-                  >
-                    <option value="Select" > 
-                      Selecciona una categoria:
-                    </option>
-                    {this.state.algo.Client.map(x =>
-                              <option value={x.username} key={x.username}>
-                                    {x.username}
-                              </option>)}
-          </Select>
-
-
-        {/* <Table id="table" border="1">
-          <th>username</th>  <th>first_name</th> <th>last_name</th>
-          <th>date_birth</th> <th>type_id</th> <th>id</th>
-          <th>phone_number</th> <th>address</th> <th>email</th>
-          <th>state</th><th>change</th>
-
-{tab} */}
-
-        {/* <tr>
-            <td >1</td>
-            <td>2</td>
-            <button>b</button>
-          </tr>
-
-          <tr>
-            <td>3</td>
-            <td>4</td>
-            <button>b</button>
-          </tr> */}
-
-        {/* </Table> */}
-
-
-
-
+        <Dashboard handleList={this.handleList} />
+        <button onClick={this.handleClick}>Clientes</button>
       </div>);
 
-
+    // <div>
+    //   <JsonToTable json={this.state.algo} />
+    //   <br />
+    //   <button id="desactivar" onClick={this.handleClick}>activate/deactivate</button>
+    //   <Select
+    //     name="categoryName"
+    //     value={this.state.status}
+    //     onChange={(x) => this.setState({ status: x.target.value })}
+    //     placeholder="Selecciona una subcategoria:"
+    //   >
+    //     <option value="Select" >
+    //       Selecciona una categoria:
+    //               </option>
+    //     {this.state.algo.Client.map(x =>
+    //       <option value={x.username} key={x.username}>
+    //         {x.username}
+    //       </option>)}
+    //   </Select>
+    // </div>);
   }
 }
 
